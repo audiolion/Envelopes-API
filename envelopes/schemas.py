d3db860339f727e485142563e8055be9742fc099
Ryan Castner <castner.rr@gmail.com>
2017-12-27 09:24:07 -0500

    Move types into types.py and add marshmallow schema into schemas

--- envelopes/schemas.py
+++ envelopes/schemas.py
@@ -1,60 +1,15 @@
 # Third Party Library Imports
-from apistar import typesystem
-
-# Local Imports
-from .models import Transaction
+from marshmallow import Schema, fields, post_load
 
 
-class AccountSchema(typesystem.Object):
-    properties = {
-        'id': typesystem.integer(minimum=1),
-        'uuid': typesystem.string(min_length=36),
-        'balance': typesystem.number(),
-        'owner_id': typesystem.integer(minimum=1),
-        'created': typesystem.string(format='date'),
-        'modified': typesystem.string(format='date'),
-    }
+class Account(Schema):
+    id = fields.Integer(min=1)
+    uuid = fields.UUID()
+    balance = fields.Decimal(places=2, required=True, as_string=True)
+    owner_id = fields.Integer(min=1, required=True, load_from='owner', dump_to='owner')
+    created = fields.DateTime()
+    modified = fields.DateTime()
 
-
-class CategorySchema(typesystem.Object):
-    properties = {
-        'name': typesystem.string(min_length=1, max_length=80),
-    }
-
-
-class EnvelopeSchema(typesystem.Object):
-    properties = {
-        'id': typesystem.integer(minimum=1),
-        'uuid': typesystem.string(min_length=36),
-        'creator': typesystem.integer(minimum=1),
-        'name': typesystem.string(min_length=1, max_length=50),
-        'description': typesystem.string(max_length=200),
-        'budget': typesystem.number(),
-        'balance': typesystem.number(),
-        'account': typesystem.integer(minimum=1),
-        'created': typesystem.string(format='date'),
-        'modified': typesystem.string(format='date'),
-    }
-
-
-class ActionTypeEnum(typesystem.Enum):
-    enum = [
-        Transaction.ACTION_TYPE_CREATED,
-        Transaction.ACTION_TYPE_DEPOSITED,
-        Transaction.ACTION_TYPE_WITHDRAWN,
-    ]
-
-
-class TransactionSchema(typesystem.Object):
-    properties = {
-        'id': typesystem.integer(minimum=1),
-        'friendly_id': typesystem.string(max_length=30),
-        'user': typesystem.integer(minimum=1),
-        'created': typesystem.string(format="date"),
-        'envelope': typesystem.integer(minimum=1),
-        'type': ActionTypeEnum,
-        'delta': typesystem.number(),
-        'description': typesystem.string(max_length=100),
-        'category': typesystem.integer(minimum=1),
-        'comment': typesystem.string(),
-    }
+    @post_load
+    def make_account(self, data):
+        return self.context['session'].Account(**data)
---
+++ envelopes/types.py
@@ -0,0 +1,60 @@
+# Third Party Library Imports
+from apistar import typesystem
+
+# Local Imports
+from .models import Transaction
+
+
+class Account(typesystem.Object):
+    properties = {
+        'id': typesystem.integer(minimum=1),
+        'uuid': typesystem.string(),
+        'balance': typesystem.number(),
+        'owner_id': typesystem.integer(minimum=1),
+        'created': typesystem.string(),
+        'modified': typesystem.string(),
+    }
+
+
+class Category(typesystem.Object):
+    properties = {
+        'name': typesystem.string(min_length=1, max_length=80),
+    }
+
+
+class Envelope(typesystem.Object):
+    properties = {
+        'id': typesystem.integer(minimum=1),
+        'uuid': typesystem.string(min_length=36),
+        'creator': typesystem.integer(minimum=1),
+        'name': typesystem.string(min_length=1, max_length=50),
+        'description': typesystem.string(max_length=200),
+        'budget': typesystem.number(),
+        'balance': typesystem.number(),
+        'account': typesystem.integer(minimum=1),
+        'created': typesystem.string(format='date'),
+        'modified': typesystem.string(format='date'),
+    }
+
+
+class ActionTypeEnum(typesystem.Enum):
+    enum = [
+        Transaction.ACTION_TYPE_CREATED,
+        Transaction.ACTION_TYPE_DEPOSITED,
+        Transaction.ACTION_TYPE_WITHDRAWN,
+    ]
+
+
+class Transaction(typesystem.Object):
+    properties = {
+        'id': typesystem.integer(minimum=1),
+        'friendly_id': typesystem.string(max_length=30),
+        'user': typesystem.integer(minimum=1),
+        'created': typesystem.string(format="date"),
+        'envelope': typesystem.integer(minimum=1),
+        'type': ActionTypeEnum,
+        'delta': typesystem.number(),
+        'description': typesystem.string(max_length=100),
+        'category': typesystem.integer(minimum=1),
+        'comment': typesystem.string(),
+    }
