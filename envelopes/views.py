8db509da957d08addcef80517efad6e6f3256da3
Ryan Castner <castner.rr@gmail.com>
2017-12-27 09:25:42 -0500

    Update views to use marshmallow schemas

--- envelopes/views.py
+++ envelopes/views.py
@@ -5,7 +5,10 @@ from apistar.interfaces import Auth
 from django.core.exceptions import ObjectDoesNotExist
 
 # Local Imports
-from .schemas import AccountSchema
+from . import schemas
+from .forms import AccountForm
+
+account_schema = schemas.Account(exclude=('id'))
 
 
 def retrieve(queryset):
@@ -27,7 +30,8 @@ def handle_error(props):
 
 def list_accounts(request: http.Request, auth: Auth, session: Session):
     queryset = session.Account.objects.filter(owner=auth.user['id'])
-    return [AccountSchema(account) for account in queryset]
+    accounts = account_schema.dump(queryset, many=True)
+    return accounts.data
 
 
 def get_account(request: http.Request, auth: Auth, session: Session, uuid):
@@ -35,23 +39,33 @@ def get_account(request: http.Request, auth: Auth, session: Session, uuid):
     props = retrieve(queryset)
     if props['error']:
         return handle_error(props)
-    return AccountSchema(props['obj'])
+    account, errors = account_schema.dump(props['obj'])
+    if errors:
+        return Response(errors, status=400)
+    return account
 
 
-def create_account(request: http.Request, auth: Auth, session: Session, data: AccountSchema):
-    account = session.Account.objects.create(**data)
-    return Response(AccountSchema(account), status=201)
+def create_account(request: http.Request, auth: Auth, session: Session, data: http.RequestData):
+    account_schema.context['session'] = session
+    if hasattr(data, 'get') and not data.get('owner'):
+        data['owner'] = auth.user['id']
+    account, errors = account_schema.load(data)
+    if errors:
+        return Response(errors, status=400)
+    account.save()
+    return Response(account_schema.dump(account).data, status=201)
 
 
-def update_account(request: http.Request, auth: Auth, session: Session, data: AccountSchema, uuid):
+def update_account(request: http.Request, auth: Auth, session: Session, data: http.RequestData, uuid):  # noqa; E501
     queryset = session.Account.objects.filter(uuid=uuid).filter(owner=auth.user['id'])
     props = retrieve(queryset)
     if props['error']:
         return handle_error(props)
-    for attr, value in data.items():
-        setattr(props['obj'], attr, value)
-    props['obj'].save()
-    return AccountSchema(props['obj'])
+    form = AccountForm(data, instance=props['obj'])
+    if form.is_valid():
+        account = form.save()
+        return account_schema.dump(account).data
+    return Response(form.errors, status=400)
 
 
 def delete_account(request: http.Request, auth: Auth, session: Session, uuid):
--- tests/test_views.py
+++ tests/test_views.py
@@ -61,7 +61,8 @@ def test_list_accounts(auth, accounts):
     client = TestClient(app)
     res = client.get('/accounts/', headers=auth['header'])
     assert res.status_code == 200
-    assert res.json() == []
+    for item in res.json():
+        assert item['owner'] == auth['user'].id
 
     auth = create_auth(accounts[0].owner)
     res = client.get('/accounts/', headers=auth['header'])
@@ -84,6 +85,67 @@ def test_get_account(auth, accounts):
     res = client.get(url, headers=auth['header'])
     assert res.status_code == 200
     assert res.json()['uuid'] == str(accounts[0].uuid)
+    with pytest.raises(KeyError):
+        res.json()['id']
 
     res = client.get(url)
     assert res.status_code == 401
+
+
+def test_create_account(auth):
+    client = TestClient(app)
+    data = {
+        'balance': '100.00',
+        'owner': auth['user'].id
+    }
+    res = client.post('/accounts/', headers=auth['header'], data=data)
+    assert res.status_code == 201
+    assert res.json()['owner'] == data['owner']
+    assert res.json()['balance'] == data['balance']
+    Account.objects.get(id=res.json()['id']).delete()
+
+    res = client.post('/accounts/', data=data)
+    assert res.status_code == 401
+
+    res = client.post('/accounts/', headers=auth['header'], data={'owner': data['owner']})
+    assert res.status_code == 400
+
+    # this test fails due to test client using an ImmutableDict and that being passed
+    # see https://github.com/encode/apistar/issues/379 for resolution
+    #
+    # res = client.post('/accounts/', headers=auth['header'], data={'balance': 50.0})
+    # assert res.status_code == 201
+    # assert res.json()['owner'] == data['owner']
+    # assert res.json()['balance'] == data['balance']
+    # Account.objects.get(id=res.json()['id']).delete()
+
+
+def test_update_account(accounts):
+    client = TestClient(app)
+    url = '/accounts/{}/'.format(accounts[0].uuid)
+    auth = create_auth(accounts[0].owner)
+    data = {'balance': accounts[0].balance + 1, 'owner': accounts[0].owner.id}
+    res = client.patch(url, headers=auth['header'], data=data)
+    assert res.status_code == 200
+    assert float(res.json()['balance']) == float(accounts[0].balance) + 1
+    uuid = res.json()['uuid']
+
+    res = client.patch(url, {'balance': 500})
+    assert res.status_code == 401
+
+    res = client.patch(url, headers=auth['header'], data={'id': 500})
+    assert res.status_code == 400
+
+    account = Account.objects.get(uuid=uuid)
+    account.balance = 100.00
+    account.save()
+
+
+def test_delete_account(accounts):
+    client = TestClient(app)
+    url = '/accounts/{}/'.format(accounts[0].uuid)
+    auth = create_auth(accounts[0].owner)
+    uuid = accounts[0].uuid
+    res = client.delete(url, headers=auth['header'])
+    assert res.status_code == 204
+    assert not Account.objects.filter(uuid=uuid).exists()
